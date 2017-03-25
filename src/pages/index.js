4e1fbce2498dcacb30e11df440b688c7be8da002
Louis Lagrange <lagrange.louis@gmail.com>
2017-03-25 17:17:15 +0100

    Rename Home file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,1 @@
-export { default as Home } from './Home';
-export { default as Boards } from './Boards';
-export { default as Board } from './Board/Board';
+export { default as Home } from './Home/Home';
