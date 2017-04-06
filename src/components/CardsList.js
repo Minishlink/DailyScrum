04e1572ee3529ece25b00d227a297f887ed73c05
Louis Lagrange <lagrange.louis@gmail.com>
2017-04-06 22:29:56 +0200

    Filter by member id

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,12 +1,13 @@
 // @flow
 import React, { Component } from 'react';
-import { View, ScrollView, RefreshControl } from 'react-native';
+import { StyleSheet, View, ScrollView, RefreshControl, Text, TouchableOpacity } from 'react-native';
 import { TrelloCard } from 'DailyScrum/src/components';
 import type { CardType } from 'DailyScrum/src/modules/cards/reducer';
+import type { ScrumbleTeamMemberType } from 'DailyScrum/src/types/Scrumble/common';
 
 export default class extends Component {
   props: PropsType;
-  state: StateType = { isRefreshing: false };
+  state: StateType = { filterableMembers: [], filteredMember: null, isRefreshing: false };
 
   // Use FlatList / SectionList when 0.43 out
 
@@ -17,10 +18,30 @@ export default class extends Component {
   // TODO on click show attachments
   // TODO on click show description
 
+  componentDidMount() {
+    this.setState({ filterableMembers: this.getFilterableMembers() });
+  }
+
   handleRefresh = () => {
     this.setState({ isRefreshing: true }, () => {
-      this.props.onRefresh().then(() => this.setState({ isRefreshing: false }));
+      this.props.onRefresh().then(() => {
+        this.setState({ isRefreshing: false, filterableMembers: this.getFilterableMembers() });
+      });
     });
+  };
+
+  getFilterableMembers = () => {
+    let members = {};
+    for (let card of this.props.cards) {
+      for (let member of card.idMembers) {
+        members[member] = true;
+      }
+    }
+    return Object.keys(members);
+  };
+
+  filterMember = (member: ScrumbleTeamMemberType) => {
+    this.setState({ filteredMember: this.state.filteredMember !== member ? member : null });
   };
 
   render() {
@@ -32,14 +53,36 @@ export default class extends Component {
         showsVerticalScrollIndicator={false}
         refreshControl={<RefreshControl refreshing={this.state.isRefreshing} onRefresh={this.handleRefresh} />}
       >
-        {cards && cards.map(card => <View key={card.idShort}><TrelloCard card={card} /></View>)}
+        <View style={styles.filterContainer}>
+          {this.state.filterableMembers.map(member => (
+            <TouchableOpacity key={member} onPress={() => this.filterMember(member)}>
+              <Text>{member} {this.state.filteredMember === member && '(selected)'}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        {cards &&
+          cards
+            .filter(card => this.state.filteredMember ? card.idMembers.includes(this.state.filteredMember) : true)
+            .map(card => <View key={card.idShort}><TrelloCard card={card} /></View>)}
       </ScrollView>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  filterContainer: {
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    borderWidth: 3,
+    borderRadius: 5,
+    borderColor: 'black',
+  },
+});
+
 type StateType = {
   isRefreshing: boolean,
+  filterableMembers: string[],
+  filteredMember: ?string,
 };
 
 type PropsType = {
