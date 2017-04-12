4f4f536bd4e9ac21bed61adea4c53c5f2778b70d
Louis Lagrange <lagrange.louis@gmail.com>
2017-04-12 21:07:03 +0200

    Sort boards by date

--- src/services/Trello.js
+++ src/services/Trello.js
@@ -1,33 +1,39 @@
-const TRELLO_APP_KEY = "***REMOVED***"; // Scrumble App Key
+const TRELLO_APP_KEY = '***REMOVED***'; // Scrumble App Key
 
 export default class {
   static getLoginURL = () => {
-    return "https://trello.com/1/authorize?" +
+    return 'https://trello.com/1/authorize?' +
       `key=${TRELLO_APP_KEY}&` +
-      "expiration=never&" +
-      "name=Daily%20Scrum&" +
-      "return_url=dailyscrum://login&" +
-      "scope=read,account";
+      'expiration=never&' +
+      'name=Daily%20Scrum&' +
+      'return_url=dailyscrum://login&' +
+      'scope=read,account';
   };
 
   static getCurrentUser = token => {
-    return fetch(`https://api.trello.com/1/members/me?fields=avatarHash,email,fullName,id,initials,username&key=${TRELLO_APP_KEY}&token=${token}`)
-      .then(res => res.json());
+    return fetch(
+      `https://api.trello.com/1/members/me?fields=avatarHash,email,fullName,id,initials,username&key=${TRELLO_APP_KEY}&token=${token}`
+    ).then(res => res.json());
   };
 
   static getBoards = token => {
-    return fetch(`https://api.trello.com/1/members/me/boards?key=${TRELLO_APP_KEY}&token=${token}`)
-    .then(res => res.json());
+    return fetch(
+      `https://api.trello.com/1/members/me/boards?filter=open&fields=name,prefs,dateLastActivity&key=${TRELLO_APP_KEY}&token=${token}`
+    )
+      .then(res => res.json())
+      .then(boards =>
+        boards.sort((a, b) => new Date(b.dateLastActivity).getTime() - new Date(a.dateLastActivity).getTime()));
   };
 
   static getBoard = (token, id) => {
-    return fetch(`https://api.trello.com/1/boards/${id}?fields=all&key=${TRELLO_APP_KEY}&token=${token}`)
-    .then(res => res.json());
+    return fetch(`https://api.trello.com/1/boards/${id}?fields=all&key=${TRELLO_APP_KEY}&token=${token}`).then(res =>
+      res.json());
   };
 
   static getCardsFromList = (token, listId) => {
-    return fetch(`https://api.trello.com/1/lists/${listId}/cards?fields=all&key=${TRELLO_APP_KEY}&token=${token}`)
-    .then(res => res.json());
+    return fetch(
+      `https://api.trello.com/1/lists/${listId}/cards?fields=all&key=${TRELLO_APP_KEY}&token=${token}`
+    ).then(res => res.json());
   };
 }
 
