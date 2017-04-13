f3c98fe013995fe225bcbe20491fc1b0da0aa952
Louis Lagrange <lagrange.louis@gmail.com>
2017-04-13 14:05:56 +0200

    Optimize Trello image width

--- src/services/adapter/BoardTrelloToAppAdapter.js
+++ src/services/adapter/BoardTrelloToAppAdapter.js
@@ -1,10 +1,11 @@
 // @flow
+import { Dimensions } from 'react-native';
 import type { BoardType } from '../../types';
 import type { TrelloBoardType } from '../../types/Scrumble';
 
 export default (board: TrelloBoardType): BoardType => {
   const image = board.prefs.backgroundImageScaled
-    ? board.prefs.backgroundImageScaled.find(image => image.width > 760).url
+    ? board.prefs.backgroundImageScaled.find(image => image.width >= Dimensions.get('window').width).url
     : null;
   return {
     id: board.id,
