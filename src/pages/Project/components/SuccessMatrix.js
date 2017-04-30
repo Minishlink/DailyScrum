9c800befbcf71c6505029d3e2cbc4ce2fa1fe06b
Louis Lagrange <lagrange.louis@gmail.com>
2017-04-30 16:05:05 +0200

    [SuccessMatrix] Use FlatList

--- src/pages/Project/components/SuccessMatrix.js
+++ src/pages/Project/components/SuccessMatrix.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 import { Icon } from '../../../components';
 import { sprintsSuccessMatrixSelector } from '../../../modules/sprints/reducer';
 import type { SprintsSuccessMatrixType } from '../../../modules/sprints/reducer';
@@ -9,36 +9,42 @@ import type { SprintsSuccessMatrixType } from '../../../modules/sprints/reducer'
 class SuccessMatrix extends Component {
   props: PropsType;
 
-  // TODO Use FlatList / SectionList when 0.43 out
+  renderRow = ({ item: sprint }) => (
+    <View style={styles.row}>
+      <Text>{sprint.number}</Text>
+      <Text>{sprint.manDays}</Text>
+      <Text>{sprint.foreseenPoints}</Text>
+      <Text>{sprint.donePoints}</Text>
+      <Text>
+        {sprint.result !== null
+          ? sprint.result
+              ? <Icon type="entypo" name="emoji-happy" size={20} color="green" />
+              : <Icon type="entypo" name="emoji-sad" size={20} color="red" />
+          : <Icon type="entypo" name="emoji-neutral" size={20} color="orange" />}
+      </Text>
+    </View>
+  );
+
+  renderHeader = () => (
+    <View style={[styles.row, styles.labelRow]}>
+      <Text style={styles.label}>#</Text>
+      <Text style={styles.label}>Man-days</Text>
+      <Text style={styles.label}>Foreseen</Text>
+      <Text style={styles.label}>Done</Text>
+      <Text style={styles.label}>OK?</Text>
+    </View>
+  );
 
   render() {
-    const { successMatrix } = this.props;
     return (
       <View style={this.props.style}>
-        <View style={[styles.row, styles.labelRow]}>
-          <Text style={styles.label}>#</Text>
-          <Text style={styles.label}>Man-days</Text>
-          <Text style={styles.label}>Foreseen</Text>
-          <Text style={styles.label}>Done</Text>
-          <Text style={styles.label}>OK?</Text>
-        </View>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          {successMatrix.map(sprint => (
-            <View key={sprint.number} style={styles.row}>
-              <Text>{sprint.number}</Text>
-              <Text>{sprint.manDays}</Text>
-              <Text>{sprint.foreseenPoints}</Text>
-              <Text>{sprint.donePoints}</Text>
-              <Text>
-                {sprint.result !== null
-                  ? sprint.result
-                      ? <Icon type="entypo" name="emoji-happy" size={20} color="green" />
-                      : <Icon type="entypo" name="emoji-sad" size={20} color="red" />
-                  : <Icon type="entypo" name="emoji-neutral" size={20} color="orange" />}
-              </Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList
+          data={this.props.successMatrix}
+          renderItem={this.renderRow}
+          keyExtractor={sprint => sprint.number}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={this.renderHeader}
+        />
       </View>
     );
   }
