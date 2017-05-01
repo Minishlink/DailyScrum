bdc955448b951d45f82326e179f8cf5abda9268d
Louis Lagrange <lagrange.louis@gmail.com>
2017-05-01 20:40:14 +0200

    [Scrumble] Catch network errors

--- src/services/Scrumble.js
+++ src/services/Scrumble.js
@@ -1,102 +1,84 @@
 import { SCRUMBLE_API_URL } from 'DailyScrum/environment';
 
 export default class {
-  static login = trelloToken => {
-    return new Promise((resolve, reject) => {
-      fetch(SCRUMBLE_API_URL + '/ScrumbleUsers/trello-login', {
-        method: 'POST',
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        }),
-        body: JSON.stringify({ trelloToken }),
-      })
-        .then(result => result.json())
-        .then(result => {
-          if (!result || !result.token) reject(result.error);
-          resolve(result.token);
-        });
-    });
-  };
+  static login = trelloToken =>
+    fetch(SCRUMBLE_API_URL + '/ScrumbleUsers/trello-login', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({ trelloToken }),
+    })
+      .then(result => result.json())
+      .then(result => {
+        if (!result || !result.token) return Promise.reject(result.error);
+        return result.token;
+      });
 
-  static getCurrentProject = token => {
-    return new Promise((resolve, reject) => {
-      fetch(SCRUMBLE_API_URL + '/Projects/current', {
-        method: 'GET',
-        headers: new Headers({
-          Authorization: token,
-        }),
-      })
-        .then(result => result.json())
-        .then(result => {
-          if (result.error) reject(result.error);
-          resolve(result);
-        });
-    });
-  };
+  static getCurrentProject = token =>
+    fetch(SCRUMBLE_API_URL + '/Projects/current', {
+      method: 'GET',
+      headers: new Headers({
+        Authorization: token,
+      }),
+    })
+      .then(result => result.json())
+      .then(result => {
+        if (result.error) return Promise.reject(result.error);
+        return result;
+      });
 
-  static getProjectByBoard = (token, boardId) => {
-    return new Promise((resolve, reject) => {
-      fetch(SCRUMBLE_API_URL + `/Projects?filter=${JSON.stringify({ where: { boardId: boardId } })}`, {
-        method: 'GET',
-        headers: new Headers({
-          Authorization: token,
-        }),
-      })
-        .then(result => result.json())
-        .then(result => {
-          if (result.error) reject(result.error);
-          resolve(result[0]);
-        });
-    });
-  };
+  static getProjectByBoard = (token, boardId) =>
+    fetch(SCRUMBLE_API_URL + `/Projects?filter=${JSON.stringify({ where: { boardId: boardId } })}`, {
+      method: 'GET',
+      headers: new Headers({
+        Authorization: token,
+      }),
+    })
+      .then(result => result.json())
+      .then(result => {
+        if (result.error) return Promise.reject(result.error);
+        return result[0];
+      });
 
-  static setCurrentProject = (token, projectId) => {
-    return new Promise((resolve, reject) => {
-      fetch(SCRUMBLE_API_URL + '/ScrumbleUsers/project', {
-        method: 'PUT',
-        headers: new Headers({
-          Authorization: token,
-          'Content-Type': 'application/json',
-        }),
-        body: JSON.stringify({ projectId }),
-      })
-        .then(result => result.json())
-        .then(result => {
-          if (result.error) reject(result.error);
-          resolve();
-        });
-    });
-  };
+  static setCurrentProject = (token, projectId) =>
+    fetch(SCRUMBLE_API_URL + '/ScrumbleUsers/project', {
+      method: 'PUT',
+      headers: new Headers({
+        Authorization: token,
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({ projectId }),
+    })
+      .then(result => result.json())
+      .then(result => {
+        if (result.error) return Promise.reject(result.error);
+        return result;
+      });
 
-  static getCurrentSprint = token => {
-    return new Promise((resolve, reject) => {
-      fetch(SCRUMBLE_API_URL + '/Sprints/active', {
-        method: 'GET',
-        headers: new Headers({
-          Authorization: token,
-        }),
-      })
-        .then(result => result.json())
-        .then(result => {
-          if (result.error) reject(result.error);
-          resolve(result);
-        });
-    });
-  };
+  static getCurrentSprint = token =>
+    fetch(SCRUMBLE_API_URL + '/Sprints/active', {
+      method: 'GET',
+      headers: new Headers({
+        Authorization: token,
+      }),
+    })
+      .then(result => result.json())
+      .then(result => {
+        if (result.error) return Promise.reject(result.error);
+        return result;
+      });
 
-  static getSprintsFromProject = (token, projectId) => {
-    return new Promise((resolve, reject) => {
-      fetch(SCRUMBLE_API_URL + `/Projects/${projectId}/sprints`, {
-        method: 'GET',
-        headers: new Headers({
-          Authorization: token,
-        }),
-      })
-        .then(result => result.json())
-        .then(result => {
-          if (result.error) reject(result.error);
-          resolve(result);
-        });
-    });
-  };
+  static getSprintsFromProject = (token, projectId) =>
+    fetch(SCRUMBLE_API_URL + `/Projects/${projectId}/sprints`, {
+      method: 'GET',
+      headers: new Headers({
+        Authorization: token,
+      }),
+    })
+      .then(result => result.json())
+      .then(result => {
+        if (result.error) return Promise.reject(result.error);
+        return result;
+      });
 }
