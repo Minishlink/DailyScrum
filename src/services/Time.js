9307e53081297b4f4372d25ec72187ef3a59f7d1
Louis Lagrange <lagrange.louis@gmail.com>
2017-07-07 21:44:53 +0200

    Set last workable day boundary to later 10:30 am

--- src/services/Time.js
+++ src/services/Time.js
@@ -2,7 +2,7 @@ const ONE_DAY = 86400 * 1000;
 
 export const getLastWorkableDayTime = () => {
   const today = new Date();
-  today.setHours(9, 0, 0, 0);
+  today.setHours(10, 30, 0, 0); // TODO customize this hour in settings
   const todayWeekNumber = today.getDay();
 
   let offsetTime = ONE_DAY; // one day
