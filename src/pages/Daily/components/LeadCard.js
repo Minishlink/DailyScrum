9c51f835c86000805efec7404a3c64e3ca515084
Louis Lagrange <lagrange.louis@gmail.com>
2017-07-23 15:43:45 +0200

    Add happy/sad sun in LeadCard

--- src/pages/Daily/components/LeadCard.js
+++ src/pages/Daily/components/LeadCard.js
@@ -1,14 +1,18 @@
 // @flow
 import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
 import { Text, Card } from 'DailyScrum/src/components';
 
 export default (props: PropsType) =>
   <Card>
-    <Text style={{ color: props.lead.points >= 0 ? 'green' : 'red' }}>
-      {`${props.lead.points >= 0 ? 'Lead' : 'Lateness'}: ${props.lead.points > 0
-        ? props.lead.points
-        : -props.lead.points} pts / ${props.lead.manDays > 0 ? props.lead.manDays : -props.lead.manDays} man-days`}
-    </Text>
+    <View style={styles.container}>
+      <Image source={{ uri: props.lead.points >= 0 ? 'sun' : 'sun_sad' }} style={styles.image} />
+      <Text style={{ color: props.lead.points >= 0 ? 'green' : 'red' }}>
+        {`${props.lead.points >= 0 ? 'Lead' : 'Lateness'}: ${props.lead.points > 0
+          ? props.lead.points
+          : -props.lead.points} pts / ${props.lead.manDays > 0 ? props.lead.manDays : -props.lead.manDays} man-days`}
+      </Text>
+    </View>
   </Card>;
 
 type PropsType = {
@@ -17,3 +21,15 @@ type PropsType = {
     manDays: number,
   },
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 40,
+    aspectRatio: 1,
+    marginRight: 10,
+  },
+});
