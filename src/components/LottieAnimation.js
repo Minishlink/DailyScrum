fa7f0596680371dc6168073992c8e298c08615c4
Louis Lagrange <lagrange.louis@gmail.com>
2017-08-27 18:11:26 +0200

    Change LottieAnimation API

--- src/components/LottieAnimation.js
+++ src/components/LottieAnimation.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { View, Animated, Platform, StyleSheet } from 'react-native';
 import Animation from 'lottie-react-native';
 
+const linearEasing = t => t;
+
 export default class LottieAnimation extends Component<void, PropsType, StateType> {
   state = {
     progress: new Animated.Value(0),
@@ -23,16 +25,16 @@ export default class LottieAnimation extends Component<void, PropsType, StateTyp
   }
 
   animate = () => {
-    const animation = this.props.animation || this.getAnimation();
+    const animation = this.props.animation ? this.props.animation(this.state.progress) : this.getDefaultAnimation();
     this.state.progress.setValue(0);
     animation.start(this.props.loop ? this.animate : null);
   };
 
-  getAnimation = () =>
+  getDefaultAnimation = () =>
     Animated.timing(this.state.progress, {
       toValue: 1,
       delay: this.props.delay,
-      easing: this.props.easing ? this.props.easing : t => t,
+      easing: this.props.easing ? this.props.easing : linearEasing,
       duration: this.props.duration || Math.round(this.props.source.op / this.props.source.fr * 1000),
       useNativeDriver: true,
     });
@@ -82,7 +84,7 @@ type StateType = {
 type PropsType = {
   style: any,
   source: Object, // require('path/to/animation.json')
-  animation?: any, // CompositeAnimation
+  animation?: Animated.Value => Object, // Animated.Value => CompositeAnimation
   loop?: boolean,
   duration?: number,
   delay?: number,
