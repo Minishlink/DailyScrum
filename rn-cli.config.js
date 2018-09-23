5b791f0963c4b8f296130124f3aa90a1fbbedc53
Louis Lagrange <lagrange.louis@gmail.com>
2018-09-23 21:32:11 +0200

    Upgrade Android

--- rn-cli.config.js
+++
@@ -1,6 +0,0 @@
-const blacklist = require('metro-config/src/defaults/blacklist');
-module.exports = {
-  getBlacklistRE() {
-    return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/]);
-  },
-};
