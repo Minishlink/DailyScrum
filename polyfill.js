105eb0229644da32a3a8731ee50f975752274ef3
Louis Lagrange <lagrange.louis@gmail.com>
2018-09-23 22:15:11 +0200

    Only polyfill on Android

--- polyfill.js
+++ polyfill.js
@@ -1,8 +1,12 @@
-// symbol polyfills
-global.Symbol = require('core-js/es6/symbol');
-require('core-js/fn/symbol/iterator');
+import { Platform } from 'react-native';
 
-// collection fn polyfills
-require('core-js/fn/map');
-require('core-js/fn/set');
-require('core-js/fn/array/find');
+if (Platform.OS === 'android') {
+  // symbol polyfills
+  global.Symbol = require('core-js/es6/symbol');
+  require('core-js/fn/symbol/iterator');
+
+  // collection fn polyfills
+  require('core-js/fn/map');
+  require('core-js/fn/set');
+  require('core-js/fn/array/find');
+}
